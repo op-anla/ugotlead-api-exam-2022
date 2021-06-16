cd455d68e7c541a00550d5608fa8f76d2183593d
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2021-06-16 21:46:15 +0200

    more mailchimp

--- express/server.js
+++ express/server.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser")
 const router = express.Router()
 var cors = require('cors')
 const mailchimp = require("@mailchimp/mailchimp_marketing");
+const fetch = require("node-fetch");
 const {
   URLSearchParams
 } = require("url");
@@ -114,14 +115,92 @@ MAILCHIMP
 // You should always store your client id and secret in environment variables for security — the exception: sample code.
 const MAILCHIMP_CLIENT_ID = process.env.MAILCHIMP_CLIENT_ID;
 const MAILCHIMP_CLIENT_SECRET = process.env.MAILCHIMP_CLIENT_SECRET;
-const BASE_URL = "http://localhost:3005";
-const OAUTH_CALLBACK = `${BASE_URL}/oauth/mailchimp/callback`;
+const BASE_URL = "http://127.0.0.1:3000";
+const OAUTH_CALLBACK = `${BASE_URL}/login/dashboard/create-campaign`;
+router.get("/test", function (req, res) {
+  res.send(
+    '<p>Welcome to the sample Mailchimp OAuth app! Click <a href="/auth/mailchimp">here</a> to log in</p>'
+  );
+});
 router.get('/auth/mailchimp', (req, res) => {
-  res.redirect(`https://login.mailchimp.com/oauth2/authorize?${querystring.stringify({
+  let url = `https://login.mailchimp.com/oauth2/authorize?${querystring.stringify({
       response_type: "code",
       client_id: MAILCHIMP_CLIENT_ID,
       redirect_uri: OAUTH_CALLBACK
-    })}`)
+  })}`
+  res.send({
+    url: url
+  });
+  // res.redirect(`https://login.mailchimp.com/oauth2/authorize?${querystring.stringify({
+  //     response_type: "code",
+  //     client_id: MAILCHIMP_CLIENT_ID,
+  //     redirect_uri: OAUTH_CALLBACK
+  // })}`)
+
+})
+
+router.get('/auth/mailchimp/login', async (req, res) => {
+  const {
+    query: {
+      code
+    }
+  } = req;
+
+  // Here we're exchanging the temporary code for the user's access token.
+  const tokenResponse = await fetch(
+    "https://login.mailchimp.com/oauth2/token", {
+      method: "POST",
+      body: new URLSearchParams({
+        grant_type: "authorization_code",
+        client_id: MAILCHIMP_CLIENT_ID,
+        client_secret: MAILCHIMP_CLIENT_SECRET,
+        redirect_uri: OAUTH_CALLBACK,
+        code
+      })
+    }
+  );
+  const {
+    access_token
+  } = await tokenResponse.json();
+  console.log(access_token);
+
+  // Now we're using the access token to get information about the user.
+  // Specifically, we want to get the user's server prefix, which we'll use to
+  // make calls to the API on their behalf.  This prefix will change from user
+  // to user.
+  const metadataResponse = await fetch(
+    "https://login.mailchimp.com/oauth2/metadata", {
+      headers: {
+        Authorization: `OAuth ${access_token}`
+      }
+    }
+  );
+
+  const {
+    dc
+  } = await metadataResponse.json();
+  console.log(dc);
+
+  // Below, we're using the access token and server prefix to make an
+  // authenticated request on behalf of the user who just granted OAuth access.
+  // You wouldn't keep this in your production code, but it's here to
+  // demonstrate how the call is made.
+
+  mailchimp.setConfig({
+    accessToken: access_token,
+    server: dc
+  });
+
+  const response = await mailchimp.ping.get();
+  console.log(response);
+
+  res.send(`
+    <p>This user's access token is ${access_token} and their server prefix is ${dc}.</p>
+
+    <p>When pinging the Mailchimp Marketing API's ping endpoint, the server responded:<p>
+
+    <code>${response}</code>
+  `);
 })
 
 module.exports = app
