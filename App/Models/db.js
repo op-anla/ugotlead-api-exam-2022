c874bf5926d8d0a35b39de6679180c362d2350ca
AndreasLagoni <46136193+AndreasLagoni@users.noreply.github.com>
2021-06-30 14:44:02 +0200

    trying to debug better

--- App/Models/db.js
+++ App/Models/db.js
@@ -9,12 +9,43 @@ const connection = mysql.createConnection({
 });
 
 // open the MySQL connection
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error('Database connection failed: ' + err.stack);
+//     return;
+//   }
+
+//   console.log('Connected to database.');
+// });
+
+
+// Connection error, try again in 2 seconds
 connection.connect(function (err) {
   if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
+    console.log('error when connecting to db:', err);
+    setTimeout(handleError, 2000);
   }
-
   console.log('Connected to database.');
 });
+// Monitor error
+connection.on('error', function (err) {
+  console.log('db error', err);
+  // If the connection is disconnected, automatically reconnect
+  if (err.code === 'PROTOCOL_CONNECTION_LOST' && err.code === 'ECONNRESET') {
+    console.log('connection lost')
+  } else {
+    throw err;
+  }
+});
+// Monitor error
+connection.on('error', function (err) {
+  console.log('db error', err);
+  // If the connection is disconnected, automatically reconnect
+  if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+    handleError();
+  } else {
+    throw err;
+  }
+});
+
 module.exports = connection;
