019862cf40686bd9fed2302ae2462022cc929f7a
Andreas Lagoni <andr46a5@edu.eal.dk>
2021-08-11 08:36:58 +0200

    Update logging.model.js

--- App/Models/logging.model.js
+++ App/Models/logging.model.js
@@ -11,44 +11,55 @@ const Logging = function (logging) {
 };
 
 Logging.findLog = (campaignId, log, result) => {
-  console.log("🚀 ~ file: logging.model.js ~ line 14 ~ log", log)
-  sql.query(`SELECT * FROM logs WHERE (campaign_id = ${campaignId} AND HTTP_USER_AGENT = '${log.user_agent}')`, (err, res) => {
-    if (err) {
-      console.log("🚀 ~ file: campaign.model.js ~ line 31 ~ sql.query ~ err", err)
-      result(err, null);
-      return;
-    }
+  console.log("🚀 ~ file: logging.model.js ~ line 14 ~ log", log);
+  sql.query(
+    `SELECT * FROM logs WHERE (campaign_id = ${campaignId} AND HTTP_USER_AGENT = '${log.user_agent}')`,
+    (err, res) => {
+      if (err) {
+        console.log(
+          "🚀 ~ file: campaign.model.js ~ line 31 ~ sql.query ~ err",
+          err
+        );
+        result(err, null);
+        return;
+      }
 
-    if (res.length) {
-      console.log("found logs: ", res);
-      result(null, res);
-      return;
-    }
+      if (res.length) {
+        console.log("found logs: ", res);
+        result(null, res);
+        return;
+      }
 
-    // not found Customer with the id
-    result({
-      kind: "not_found"
-    }, null);
-  });
+      // not found log with the specific HTTP USER AGENT
+      result(
+        {
+          kind: "not_found",
+        },
+        null
+      );
+    }
+  );
 };
 Logging.create = (newLog, result) => {
   sql.query("INSERT INTO logs SET ?", newLog, (err, res) => {
     if (err) {
-      console.log("🚀 ~ file: campaign.model.js ~ line 13 ~ sql.query ~ err", err)
+      console.log(
+        "🚀 ~ file: campaign.model.js ~ line 13 ~ sql.query ~ err",
+        err
+      );
       result(err, null);
       return;
     }
 
     console.log("created log: ", {
       id: res.insertId,
-      ...newLog
+      ...newLog,
     });
 
     result(null, {
       id: res.insertId,
-      ...newLog
+      ...newLog,
     });
-
   });
 };
 module.exports = Logging;
