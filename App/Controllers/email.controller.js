eb5b9976e52de446689975159e0a9abbfb367df1
Andreas Lagoni <andr46a5@edu.eal.dk>
2021-08-17 15:02:30 +0200

    Update email.controller.js

--- App/Controllers/email.controller.js
+++ App/Controllers/email.controller.js
@@ -3,38 +3,32 @@ const nodemailer = require("nodemailer");
 
 exports.sendTest = (req, res) => {
   // async..await is not allowed in global scope, must use a wrapper
-  async function main() {
-    // Generate test SMTP service account from ethereal.email
-    // Only needed if you don't have a real mail account for testing
-    let testAccount = await nodemailer.createTestAccount();
-
-    // create reusable transporter object using the default SMTP transport
+  async function sendTest() {
     let transporter = nodemailer.createTransport({
-      host: "smtp.ethereal.email",
+      host: "172.20.221.3",
       port: 587,
       secure: false, // true for 465, false for other ports
-      auth: {
-        user: testAccount.user, // generated ethereal user
-        pass: testAccount.pass, // generated ethereal password
+    });
+
+    transporter.sendMail(
+      {
+        from: "no-reply@api.ugotlead.dk",
+        to: "anla@onlineplus.dk",
+        subject: "Message",
+        text: "I hope this message gets delivered!",
       },
-    });
-
-    // send mail with defined transport object
-    let info = await transporter.sendMail({
-      from: '"Fred Foo 👻" <foo@example.com>', // sender address
-      to: "bar@example.com, baz@example.com", // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
-
-    console.log("Message sent: %s", info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      (err, info) => {
+        console.log(info);
+        console.log(err);
+      }
+    );
   }
 
-  main().catch(console.error);
+  sendTest()
+    .then((data) => {
+      return res.status(200).send("Working");
+    })
+    .catch((e) => {
+      return res.status(400).send("Didn't work");
+    });
 };
