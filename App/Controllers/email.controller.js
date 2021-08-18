c9a4e0217a5fc7174b4b2a5eb047bef473e735d0
Andreas Lagoni <andr46a5@edu.eal.dk>
2021-08-18 11:44:02 +0200

    lidt mere mail

--- App/Controllers/email.controller.js
+++ App/Controllers/email.controller.js
@@ -5,18 +5,21 @@ exports.sendTest = (req, res) => {
   // async..await is not allowed in global scope, must use a wrapper
   async function sendTest() {
     let transporter = nodemailer.createTransport({
-      host: "api.ugotlead.dk",
+      host: "vm0349.enterprisecloud.nu",
       port: 465,
       secure: true, // true for 465, false for other ports,
+      auth: {
+        user: "no-reply@ugotlead.dk",
+        pass: "Xav62115?",
+      },
     });
     /* 
-Inbound email is working fine with Plesk but outbound mails is not working.
-Usually it gives Relay Access Denied
-*/
+    Inbound email is working fine with Plesk but outbound mails is not working.
+    */
     transporter.sendMail(
       {
-        from: "no-reply@api.ugotlead.dk",
-        to: "test@api.ugotlead.dk",
+        from: "no-reply@ugotlead.dk",
+        to: "anla@onlineplus.dk",
         subject: "Message",
         text: "I hope this message gets delivered!",
       },
--- server-local.js
+++ server-local.js
@@ -1,4 +1,4 @@
-'use strict';
-const app = require('./express/server');
+"use strict";
+const app = require("./express/server");
 
-app.listen(3005, () => console.log('Local app listening on port 3005!'));
+app.listen(3008, () => console.log("Local app listening on port 3008!"));
