91efe20cf8cfb1afc8ad5b600be4fc8277d6ecc6
Andreas Lagoni <anla@onlineplus.dk>
2021-09-02 10:11:01 +0200

    Update GoogleAuth.controller.js

--- App/Controllers/GoogleAuth.controller.js
+++ App/Controllers/GoogleAuth.controller.js
@@ -4,21 +4,42 @@ const { URLSearchParams } = require("url");
 const { encrypt, decrypt } = require("../common/middleware/crypto");
 
 exports.login = async (req, res) => {
-  console.log("We got the authorization code", req.query.code);
   /* Lets do the OAUTH 2.0 Google endpoint to get the access_token */
-  const OAUTH_CALLBACK = "http://127.0.0.1:3008/v1/api/google/login";
-  const code = req.query.code;
-  const tokenResponse = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
+  console.log("request? ", req.query);
+  const OAUTH_CALLBACK = "http://127.0.0.1:3008/v1/api/google/auth";
+  const {
+    query: { code },
+  } = req;
+  console.log("We got the authorization code", code);
 
-    body: new URLSearchParams({
-      grant_type: "authorization_code",
-      client_id: process.env.GOOGLE_CLIENTID,
-      client_secret: process.env.GOOGLE_CLIENTSECRET,
-      redirect_uri: OAUTH_CALLBACK,
-      code: code,
-    }),
-  });
-  console.log("tokenResponse", tokenResponse);
-  res.status(200);
+  const token = async () => {
+    try {
+      return await fetch("https://oauth2.googleapis.com/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+        },
+        body: new URLSearchParams({
+          grant_type: "authorization_code",
+          client_id: process.env.GOOGLE_CLIENTID,
+          client_secret: process.env.GOOGLE_CLIENTSECRET,
+          redirect_uri: OAUTH_CALLBACK,
+          code: code,
+        }),
+      })
+        .then((res) => {
+          return res.json();
+        })
+        .catch((err) => {
+          console.log("token ~ err", err);
+          return err;
+        });
+    } catch (err) {
+      console.log("token ~ err", err);
+      return err;
+    }
+  };
+  const tokenResponse = await token();
+  console.log("exports.login= ~ tokenResponse", tokenResponse);
+  res.redirect("http://127.0.0.1:3000/");
 };
