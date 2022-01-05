0eef9c419e92e6e3016429e2eef45b1111ff870c
Andreas Lagoni <anla@onlineplus.dk>
2022-01-05 11:28:31 +0100

    Update request.validation.middleware.js

--- App/common/middleware/request.validation.middleware.js
+++ App/common/middleware/request.validation.middleware.js
@@ -3,40 +3,42 @@ exports.validateDomain = (req, res, next) => {
   if (user_agent.includes("Postman")) {
     return next();
   }
-  const acceptedVariables = {
-    host: [
-      "127.0.0.1:3008",
-      "127.0.0.1:3000",
-      "app.ugotlead.dk/",
-      "app.ugotlead.dk",
-      "exam.app.ugotlead.dk",
-      "exam.app.ugotlead.dk/",
-      "127.0.0.1:8000",
-      "127.0.0.1:8000",
-      "127.0.0.1:8005",
-      "api.ugotlead.dk",
-    ],
-    origin: [
-      "http://127.0.0.1:8000",
-      "localhost:8000",
-      "http://localhost:8000",
-      "http://127.0.0.1:8000",
-      "https://app.ugotlead.dk",
-      "https://app.ugotlead.dk/",
-    ],
-  };
-  if (acceptedVariables.host.includes(req.headers.host)) {
-    console.log("Accepted host");
-    return next();
-    if (acceptedVariables.origin.includes(req.headers.origin)) {
-      console.log("Accepted origin");
-      return next();
-    } else {
-      console.log("REJECTED ORIGIN");
-      res.status(401).send("Not auth from this domain");
-    }
-  } else {
-    console.log("REJECTED HOST");
-    res.status(401).send("Not auth from this domain");
-  }
+  // Exam - So just return next
+  return next();
+  // const acceptedVariables = {
+  //   host: [
+  //     "127.0.0.1:3008",
+  //     "127.0.0.1:3000",
+  //     "app.ugotlead.dk/",
+  //     "app.ugotlead.dk",
+  //     "exam.app.ugotlead.dk",
+  //     "exam.app.ugotlead.dk/",
+  //     "127.0.0.1:8000",
+  //     "127.0.0.1:8000",
+  //     "127.0.0.1:8005",
+  //     "api.ugotlead.dk",
+  //   ],
+  //   origin: [
+  //     "http://127.0.0.1:8000",
+  //     "localhost:8000",
+  //     "http://localhost:8000",
+  //     "http://127.0.0.1:8000",
+  //     "https://app.ugotlead.dk",
+  //     "https://app.ugotlead.dk/",
+  //   ],
+  // };
+  // if (acceptedVariables.host.includes(req.headers.host)) {
+  //   console.log("Accepted host");
+  //   return next();
+  //   if (acceptedVariables.origin.includes(req.headers.origin)) {
+  //     console.log("Accepted origin");
+  //     return next();
+  //   } else {
+  //     console.log("REJECTED ORIGIN");
+  //     res.status(401).send("Not auth from this domain");
+  //   }
+  // } else {
+  //   console.log("REJECTED HOST");
+  //   res.status(401).send("Not auth from this domain");
+  // }
 };
